ab67e301fddbf80fe67dc2919961b738fd220213
ezequiel <5582340@alu.murciaeduca.es>
2022-12-12 17:06:39 +0100

    vistas y layout

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('paginaprincipal');
+});
+Route::get('login', function () {
+    return view('login');
+});
+Route::get('logout', function () {
+    return view('logout');
+});
+Route::get('productos', function () {
+    return view('listaproductos');
+});
+Route::get('productos/show/{id}', function () {
+    return view('mostrarproducto');
+});
+Route::get('productos/create', function () {
+    return view('añadirproducto');
+});
+Route::get('productos/edit/{id}', function () {
+    return view('editarproducto');
+});
