b6f2eddc2b8e5d072424a04374532ee2b272d418
Joche <5514134@alu.murciaeduca.es>
2023-01-11 08:50:33 +0100

    BaseDeDatosActualizado

---
+++ database/migrations/2023_01_11_070345_create_lugar_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLugarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lugares', function (Blueprint $table) {
+            $table->string('nombreLugar');
+            $table->text('direccion')->unique();
+            $table->enum('tipoLugar',['Tienda','Zona de recogida','Tienda y zona de recogida']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lugares');
+    }
+}
