df505f5283e3929c85cc2103f4ff4b44c57499d1
ezequiel <5582340@alu.murciaeduca.es>
2023-01-31 08:51:45 +0100

    creacion de middleware

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,8 +14,9 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $numElementos = $request->input('numElementos');
         return UserResource::collection(User::paginate());
     }
 
