90280a097ca6c7e9bfeb93c1044189d0f3f210a3
ezequiel <5582340@alu.murciaeduca.es>
2023-02-01 09:47:17 +0100

    auntenticacion con tokens

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Tqdev\PhpCrudApi\Api;
 use Tqdev\PhpCrudApi\Config\Config;
 use App\Http\Controllers\API\CustomerController;
 use App\Http\Controllers\API\UserController;
-
+use App\Http\Controllers\API\TokenController;
 use App\Http\Controllers\API\ArtworkController;
 
 /*
@@ -20,13 +20,20 @@ use App\Http\Controllers\API\ArtworkController;
 |
 */
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    $user = $request->user();
+    $user->fullName = $user->name;
+    return $user;
 });
 
 Route::get('artworks', [ArtworkController::class, 'index']);
 
 Route::apiResource('customers', CustomerController::class);
 Route::apiResource('users', UserController::class);
+
+// emite un nuevo token
+Route::post('tokens', [TokenController::class, 'store']);
+// elimina el token del usuario autenticado
+Route::delete('tokens', [TokenController::class, 'destroy'])->middleware('auth:sanctum');
 
 // Route::get('shoppings', [ShoppingController::class, 'index']);
 
