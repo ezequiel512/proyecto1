e704a918aeecf4a5e247951406d47c8595d76f33
José Enrique <5514134@alu.murciaeduca.es>
2023-02-07 10:50:08 +0100

    Creación roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Customer;
 use App\Models\User;
 use App\Models\Order;
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -26,12 +27,22 @@ class DatabaseSeeder extends Seeder
         DB::table('orders')->truncate();
         DB::table('customers')->truncate();
         DB::table('users')->truncate();
+        DB::table('roles')->truncate();
+        DB::table('role_user')->truncate();
 
         User::create([
             'name' => env('DATABASE_ADMIN'),
             'email' => env('DATABASE_EMAIL'),
             'password' => Hash::make(env('DATABASE_PASS')),
             'email_verified_at' => now()
+        ]);
+
+        Role::create([
+            'name' => 'Admin',
+        ]);
+
+        Role::create([
+            'name' => 'customer',
         ]);
 
         User::factory(10)
