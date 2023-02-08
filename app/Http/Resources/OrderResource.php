59d0c98875b5a3c6c284cbddeee7b397854197cc
José Enrique <5514134@alu.murciaeduca.es>
2023-02-08 09:55:34 +0100

    Creando un test y el método isAdmin del modelo User

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -14,6 +14,9 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'attributes' => parent::toArray($request)
+        ];
     }
 }
